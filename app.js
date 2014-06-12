402eb28458b4d003641fe2130e4e43b1b9a3dc6a
Jaime Victoria <jaime.victoria@crowdint.com>
2014-06-12 16:19:35 -0500

    added directives to show or hide product and purchase button

--- app.js
+++ app.js
@@ -2,9 +2,9 @@
   var app = angular.module('gemStore', []);
 
   var gems = [
-    { name: 'Azurite', price: 2.95 },
-    { name: 'Bloodstone', price: 5.95 },
-    { name: 'Zircon', price: 3.95 },
+    { name: 'Azurite', price: 2.95, soldOut: true, canPurchase: false },
+    { name: 'Bloodstone', price: 5.95, soldOut: false, canPurchase: true },
+    { name: 'Zircon', price: 3.95, soldOut: false, canPurchase: false },
   ];
 
   app.controller('StoreController', function() {
