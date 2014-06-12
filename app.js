ff7c5092f8b3ebdf04518cd6a6c7e9100c5a4b52
Jaime Victoria <jaime.victoria@crowdint.com>
2014-06-12 13:26:19 -0500

    iterate products and render them

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 (function(){
-  var gem = { name: 'Azurite', price: 3.95 };
   var app = angular.module('gemStore', []);
 
+  var gems = [
+    { name: 'Azurite', price: 2.95 },
+    { name: 'Bloodstone', price: 5.95 },
+    { name: 'Zircon', price: 3.95 },
+  ];
+
   app.controller('StoreController', function() {
-  	this.product = gem;
+  	this.products = gems;
   });
+
 })();
+
